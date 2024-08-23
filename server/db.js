73eba328428d9decfe788b50c9feb40658395a4a
Janardhan_Guntaka <2003janardhanreddyguntaka@gmail.com>
2024-08-22 20:43:44 -0400

    updated in new mac

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const pool=new Pool({
     user:'postgres',
-    password:'Janardhan@9',
+    password:'Janardhan@30351',
     host: 'localhost',
     port:5432,
     database: 'todoapp'
